8e64c1453eae4fbc7d682fd2da152646b5039f3e
llsonyll <sony_s07@hotmail.es>
2022-09-28 19:49:47 -0500

    feat: home background switch logic and styles

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
+
 import Header from "../header";
 import OutlinedButton from "../OutLinedBtn";
 import "./home.scss";
 
-const imgUrl_1 =
-  "http://songoftheroad.com/wp-content/uploads/2014/12/PeruParacasDay2RedBeachSam.jpg";
+const images = [
+  "http://songoftheroad.com/wp-content/uploads/2014/12/PeruParacasDay1CampSpotXPCamperCliffsBeach.jpg",
+  "http://songoftheroad.com/wp-content/uploads/2014/12/PeruParacasDay2RedBeachSam.jpg",
+  "http://songoftheroad.com/wp-content/uploads/2014/12/PeruParacasDay1EricaAtCampsiteChillaxes.jpg",
+];
 
 const Home = () => {
+  const [bgImg, setBgImg] = useState(0);
+
+  const bgStyles = {
+    transform: `translateX(-${bgImg * 100}%)`,
+  };
+
+  const handleSwitchBg = (id) => {
+    if (id === bgImg) return;
+    setBgImg(id);
+  };
+
   return (
     <div className="home">
       <Header />
       <div className="home__content">
         <h1 className="title">
-          ESTÁR CÓMODO, <br /> NUNCA FUE TAN FÁCIL{" "}
+          ESTÁR CÓMODO, <br /> NUNCA FUE TAN FÁCIL.
         </h1>
 
         <div className="action">
@@ -19,9 +35,29 @@ const Home = () => {
         </div>
       </div>
       <div className="switch">
-        <button> 1 </button>
-        <button>2 </button>
-        <button>3</button>
+        {images.map((_, index) => {
+          return (
+            <button
+              key={`switch-btn-${index}`}
+              onClick={() => handleSwitchBg(index)}
+            >
+              {index + 1}
+            </button>
+          );
+        })}
+      </div>
+      <div className="background">
+        {images.map((image, index) => {
+          return (
+            <div
+              key={`bg-image-${index}`}
+              className="bg-image"
+              style={bgStyles}
+            >
+              <img src={image} alt="bg-1" />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
