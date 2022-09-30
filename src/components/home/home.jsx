e2a19adf871f0f06e4fe1583d3a5840033dc9dbf
llsonyll <sony_s07@hotmail.es>
2022-09-30 13:36:41 -0500

    fix: image sources for brackground from http to https

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -6,9 +6,9 @@ import OutlinedButton from "../OutLinedBtn";
 import "./home.scss";
 
 const images = [
-  "http://songoftheroad.com/wp-content/uploads/2014/12/PeruParacasDay1CampSpotXPCamperCliffsBeach.jpg",
-  "http://songoftheroad.com/wp-content/uploads/2014/12/PeruParacasDay2RedBeachSam.jpg",
-  "http://songoftheroad.com/wp-content/uploads/2014/12/PeruParacasDay1EricaAtCampsiteChillaxes.jpg",
+  "https://www.peru.travel/Contenido/Experiencia/Imagen/en/79/1.1/Sighting%20of%20flora%20and%20fauna%20in%20the%20PN%20Manu.jpg",
+  "https://planetofhotels.com/guide/sites/default/files/styles/paragraph__live_banner__lb_image__1880bp/public/live_banner/paracas.jpg",
+  "https://media.npr.org/assets/img/2021/08/09/gettyimages-1229409136-db9b54a1546b7f503c7e812178f4cab5b707aca3.jpg",
 ];
 
 const Home = () => {
